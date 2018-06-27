81f5b79a8c9a0471c297c8cdeda7f251dc7fabac
Iago Virgílio <iagojm@hotmail.com>
2018-06-26 22:28:07 -0300

    atualização efeito javascript

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,7 @@ var passos = document.getElementById('passos');
 passos.addEventListener("mouseover", function(){
     displayPassos.id= 'block';
     displayPassos.classList.remove("none");
-    displayPassos.className='fadeIn';
+    displayPassos.classList.add("fadeIn");
 })
 
 // Ativar fade in Vantagens
@@ -14,7 +14,6 @@ var vantagens = document.getElementById("vantagens");
 
 vantagens.addEventListener("mouseover", function(){
     displayVantagens.id= 'block';
-    displayVantagens.classList.add("fadeIn");
     displayVantagens.classList.remove("none");
-    
+    displayVantagens.classList.add("fadeIn");
 })
